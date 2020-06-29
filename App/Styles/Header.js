e9d14c20fa1028a900385eed828f38c256d2e040
ahtsham Shoukat <ahtxham@gmail.com>
2020-06-29 19:48:35 +0500

    first commit

---
+++ App/Styles/Header.js
@@ -0,0 +1,29 @@
+import { Colors } from "../Themes/"
+
+const Styles = {
+    header: {
+        backgroundColor: Colors.HeaderColor,
+    },
+    logo: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+    },
+    rightImage: {
+        width: 30,
+        height: 30,
+    },
+    leftImage: {
+        width: 25,
+        height: 25,
+    },
+    title: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    }
+
+
+};
+
+export default Styles;
